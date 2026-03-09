23d7969a4e29491f274a1108737289586dc907de
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-03-10 00:14:37 +0300

    Final Point.h

--- Data_Structure/Task_T4_Shape/Point.h
+++ Data_Structure/Task_T4_Shape/Point.h
@@ -2,18 +2,34 @@
 #define POINT_H
 
 #include <iostream>
+#include <cmath>
 
+struct Point {
+    double x;
+    double y;
+    Point() : x(0.0), y(0.0) {}
+    Point(double x_val, double y_val) : x(x_val), y(y_val) {}
 
-class Point {
-private:
-    
+    double distanceTo(const Point& other) const {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return std::sqrt(dx*dx + dy*dy);
+    }
 
-public:
+    Point operator+(const Point& other) const {
+        return Point(x + other.x, y + other.y);
+    }
+    Point operator-(const Point& other) const {
+        return Point(x - other.x, y - other.y);
+    }
+    Point operator*(double scalar) const {
+        return Point(x*scalar, y*scalar);
+    }
 
+    friend std::ostream& operator<<(std::ostream& os, const Point& p) {
+        os << "(" << p.x << ", " << p.y << ")";
+        return os;
+    } 
 } ;
 
-
-
-
-
-#endif
+#endif // POINT_H
