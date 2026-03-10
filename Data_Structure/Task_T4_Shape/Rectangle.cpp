f8422f6f78ef794e956339d0a9db311eee4f1271
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-03-10 23:49:18 +0300

    Added checkout about common things

--- Data_Structure/Task_T4_Shape/Rectangle.cpp
+++ Data_Structure/Task_T4_Shape/Rectangle.cpp
@@ -2,7 +2,24 @@
 #include <string>
 #include "Rectangle.h"
 
-Rectangle::Rectangle(const Point& bl, const Point& tr) : bottomLeft(bl), topRight(tr) {}
+Rectangle::Rectangle(const Point& bl, const Point& tr) : bottomLeft(bl), topRight(tr) {
+    isValid();
+}
+
+void Rectangle::isValid() const {
+    if (bottomLeft.x >= topRight.x) {
+        throw std::invalid_argument("Rectangle: left x must be less than right x");
+    }
+    if (bottomLeft.y >= topRight.y) {
+        throw std::invalid_argument("Rectangle: left y must be less than right y");
+    }
+
+    double width = topRight.x - bottomLeft.x;
+    double height = topRight.y - bottomLeft.y;
+    if ((width * height) <= 0) {
+        throw std::invalid_argument("Rectangle: area must be positive");
+    }
+}
 
 double Rectangle::getArea() const {
     double width = topRight.x - bottomLeft.x;
@@ -25,6 +42,9 @@ void Rectangle::move(double dx, double dy) {
 }
 
 void Rectangle::scale(double fact) {
+    if (fact <= 0) {
+        throw std::invalid_argument("Scale factor must be positive");
+    }
     Point center = getCenter();
     bottomLeft.x = center.x + (bottomLeft.x - center.x) * fact;
     bottomLeft.y = center.y + (bottomLeft.y - center.y) * fact;
