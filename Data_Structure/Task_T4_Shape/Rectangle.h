de2c7f7856f4965c3b79841821b58152f1452323
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-03-10 23:27:33 +0300

    Created Rectangle.h

---
+++ Data_Structure/Task_T4_Shape/Rectangle.h
@@ -0,0 +1,23 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include <iostream>
+#include <string>
+#include "Point.h"
+#include "Shape.h"
+
+class Rectangle : public Shape {
+private:
+    Point bottomLeft;
+    Point topRight;
+public:
+    Rectangle(const Point& bl, const Point& tr);
+
+    double getArea() const override;
+    Point getCenter() const override;
+    void move(double dx, double dy) override;
+    void scale(double fact) override;
+    std::string getName() const override;
+} ; 
+
+#endif // RECTANGLE_H
