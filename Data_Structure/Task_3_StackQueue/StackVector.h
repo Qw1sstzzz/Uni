130ab523374a062246bdedd1d1ec4ae0ce87013e
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-03-18 01:37:42 +0300

    Created Stackvector.h

---
+++ Data_Structure/Task_3_StackQueue/StackVector.h
@@ -0,0 +1,28 @@
+#ifndef STACK_VECTOR_H
+#define STACK_VECTOR_H
+
+#include <iostream>
+#include "Stack.h"
+
+template <typename T>
+class StackVector : public Stack<T> {
+private:
+    T* data;
+    size_t capacity;
+    int topIndex;
+public:
+    StackVector() : data(nullptr), capacity(0), topIndex(-1) {}
+    StackVector(size_t size) : data(nullptr), capacity(size), topIndex(-1) {}
+    StackVector(const StackVector& other);
+    StackVector(StackVector&& other);
+    ~StackVector();
+
+    StackVector& operator=(const StackVector& other);
+    StackVector& operator=(StackVector&& other);
+
+    void push(const T& e) override;
+    T pop() override;
+    bool isEmpty() override;
+} ;
+
+#endif // STACK_VECTOR_H
