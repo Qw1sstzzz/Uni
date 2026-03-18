9d6159292f6a283ad7f607b6a30e8c99dcb20aa9
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-03-19 00:45:22 +0300

    Small changes in Queue.h

--- Data_Structure/Task_3_StackQueue/Queue.h
+++ Data_Structure/Task_3_StackQueue/Queue.h
@@ -10,7 +10,7 @@ public:
     virtual ~Queue() {}
     virtual void enQueue(const T& el) = 0;
     virtual T deQueue() = 0;
-    virtual bool isEmpty() = 0;
+    virtual bool isEmpty() const = 0;
 } ;
 
 #endif //QUEUE_H
