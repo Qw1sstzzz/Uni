954f08b929639876ac9384e37921be27789b551e
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-03-18 23:41:33 +0300

    Redesigned func isEmpty()

--- Data_Structure/Task_3_StackQueue/Stack.h
+++ Data_Structure/Task_3_StackQueue/Stack.h
@@ -9,7 +9,7 @@ class Stack {
 public:
     virtual void push(const T& e) = 0;
     virtual T pop() = 0;
-    virtual bool isEmpty() = 0;
+    virtual bool isEmpty() const = 0;
     virtual ~Stack() {};
 } ;
 
