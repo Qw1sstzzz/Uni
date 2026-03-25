6bc1ec792bbad1d03898d8595bfb094b16ec9d1f
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-03-25 23:53:58 +0300

    Started, but understand, that deadline is too far)

---
+++ Data_Structure/Task_T2_Iterators/DataStruct.h
@@ -0,0 +1,16 @@
+#ifndef DATA_STRUCT_H
+#define DATA_STRUCT_H
+
+#include <iostream>
+#include <string>
+#include <complex>
+#include <utility>
+
+struct DataStruct {
+    double key1;
+    unsigned long long key2;
+    std::string key3;
+} ;
+
+
+#endif // DATA_STRUCT_H
