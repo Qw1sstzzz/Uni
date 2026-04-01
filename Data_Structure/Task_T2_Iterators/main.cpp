db102b6380b1c12dbe8ed76997ef34b1eb8f23a2
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-04-02 02:26:42 +0300

    Added a class for sving formated cin

--- Data_Structure/Task_T2_Iterators/main.cpp
+++ Data_Structure/Task_T2_Iterators/main.cpp
@@ -216,6 +216,30 @@ std::istream& operator>>(std::istream& in, DataStruct& dest) {
 }
 
 
+class iofmtguard {
+private:
+    std::basic_ios<char>& s_;
+    std::streamsize width_;
+    char fill_;
+    std::streamsize precision_;
+    std::ios::fmtflags fmt_;
+public:
+    iofmtguard(std::basic_ios<char>& s) : s_(s) {
+        width_ = s.width();
+        fill_ = s.fill();
+        precision_ = s.precision();
+        fmt_ = s.flags();
+    }
+
+    ~iofmtguard() {
+        s_.width(width_);
+        s_.fill(fill_);
+        s_.precision(precision_);
+        s_.flags(fmt_);
+    }
+} ;
+
+
 int main(void) {
     std::cout << "Program started" << std::endl;
     
