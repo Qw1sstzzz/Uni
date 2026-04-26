229e7daea2790718a998c2faf4f579498cb26b79
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-04-26 21:17:24 +0300

    Created BST.h

---
+++ Data_Structure/Preparation_for_Control_Point_2/BST.h
@@ -0,0 +1,43 @@
+#ifndef BST_H
+#define BST_H
+
+struct BalanceReport {
+    int missingLeft = 0;
+    int missingRight = 0;
+    int bothChildren = 0;
+    int total = 0;
+} ;
+
+template <typename T>
+class BinarySearchTree {
+private:
+    struct Node {
+        T key_;
+        Node* left_;
+        Node* right_;
+        Node* p_;
+
+        Node(T key, Node* left = nullptr, Node* right = nullptr, Node* p = nullptr) :
+            key_(key), left_(left), right_(right), p_(p) {}
+    } ;
+
+    Node* root_;
+
+    void clear(Node* node);
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+
+    BinarySearchTree(const BinarySearchTree<T>& other) = delete;
+    BinarySearchTree& operator=(const BinarySearchTree<T>& other) = delete;
+
+    // BinarySearchTree(BinarySearchTree<T>&& other);
+    // BinarySearchTree& operator=(BinarySearchTree<T>&& other);
+
+    bool insert(const T& key);
+    void output(std::ostream& out) const;
+    BalanceReport findImbalancedNodes() const;
+} ;
+
+
+#endif //BST_H
