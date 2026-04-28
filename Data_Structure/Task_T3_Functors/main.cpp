7223f0aac4ed8c36052354ea97d89133038f8f70
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-04-29 01:39:49 +0300

    Created Polygon and oper==

--- Data_Structure/Task_T3_Functors/main.cpp
+++ Data_Structure/Task_T3_Functors/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <functional>
 
 
@@ -11,6 +12,17 @@ bool operator==(const Point& a, const Point& b) {
     return ((a.x == b.x) && (a.y == b.y));
 }
 
+
+struct Polygon {
+    std::vector<Point> points;
+} ;
+
+bool operator==(const Polygon& a, const Polygon& b) {
+    if (a.points.size() != b.points.size()) {
+        return false;
+    }
+    return std::equal(a.points.begin(), a.points.end(), b.points.begin());
+}
 
 int main(void) {
 
