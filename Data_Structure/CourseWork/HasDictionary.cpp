826732b52b27737052c3b1b1327da1284164664d
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-05-04 22:52:43 +0300

    Added Constructor + Destructor + func isPrime

---
+++ Data_Structure/CourseWork/HasDictionary.cpp
@@ -0,0 +1,38 @@
+#include "HashDictionary.h"
+
+
+HashDictionary::HashDictionary(size_t size) {
+    size_ = nextPrime(size);
+    table_ = new Line[size_];
+    count_ = 0;
+    collisions_ = 0;
+
+    for (size_t i = 0; i < size_; ++i) {
+        table_[i].status_ = EMPTY;
+    }
+}
+
+
+HashDictionary::~HashDictionary() {
+    delete[] table_;
+}
+
+
+bool HashDictionary::isPrime(size_t n) {
+    if (n < 2) {
+        return false;
+    }
+    if (n == 2) {
+        return true;
+    }
+    if (n % 2 == 0) {
+        return false;
+    }
+
+    for (size_t i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
