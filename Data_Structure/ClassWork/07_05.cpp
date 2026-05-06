c21d01e05519972b12a8111e27ec71c8ba5cb349
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-05-07 00:56:52 +0300

    Tried to prepare for Control Work

--- Data_Structure/ClassWork/07_05.cpp
+++ Data_Structure/ClassWork/07_05.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <numeric>
 #include <vector>
+#include <string>
 
 using namespace std::placeholders;
 
@@ -42,5 +43,75 @@ int main(void) {
 
 
     int sum = std::accumulate(v.begin(), v.end(), 0);
-    std::cout << sum << std::endl;
+    int pr = std::accumulate(v.begin(), v.end(), 1, std::multiplies<int>());
+
+    std::cout << sum << std::endl << pr << std::endl;
+
+    std::vector<std::string> words{"cat", "dog", "bird"};
+    std::vector<int> lengths;
+    std::transform(words.begin(), words.end(),
+        std::back_inserter(lengths),
+        [](const std::string& s) {
+            return s.length();
+    });
+
+    for (int len : lengths) {
+        std::cout << len << ' ';
+    }
+    std::cout << std::endl;
 }
+
+
+/*
+std::vector<std::string> filtered;
+std::copy_if(words.begin(), words.end(),
+    std::back_inserter(filtered),
+    [](const std::string& s) { return s.length() <= 3; });
+
+std::vector<char> first_chars;
+std::transform(filtered.begin(), filtered.end(),
+    std::back_inserter(first_chars),
+    [](const std::string& s) { return s[0]; });
+
+
+
+
+int total_length = std::accumulate(words.begin(), words.end(), 0,
+    [](int acc, const std::string& s) {
+    return acc + (s.length() <= 3 ? s.length() : 0);
+});
+
+
+
+std::vector<int> lengths;
+std::transform(words.begin(), words.end(),
+    std::back_inserter(lengths),
+    [](const std::string& s) { return s.length();
+});
+
+auto min_it = std::min_element(lengths.begin(), lengths.end());
+auto max_it = std::max_element(lengths.begin(), lengths.end());
+
+
+
+std::vector<std::string> short_words;
+std::copy_if(words.begin(), words.end(),
+    std::back_inserter(short_words),
+    [](const std::string& s) { return s.length() <= 3; });
+
+
+
+auto it = std::find_if(words.begin(), words.end(),
+    [](const std::string& s) { return !s.empty() && s[0] == 'c'; });
+
+
+
+auto shortest = std::min_element(words.begin(), words.end(),
+    [](const std::string& a, const std::string& b) {
+        return a.length() < b.length();
+    });
+auto longest = std::max_element(words.begin(), words.end(),
+    [](const std::string& a, const std::string& b) {
+        return a.length() < b.length();
+    });
+*/
