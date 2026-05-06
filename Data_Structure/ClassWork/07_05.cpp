1822758c35b8e979424c55543c317ae7ba9d2d0c
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-05-07 00:37:20 +0300

    Understanding functors

--- Data_Structure/ClassWork/07_05.cpp
+++ Data_Structure/ClassWork/07_05.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <functional>
+#include <algorithm>
+#include <numeric>
+#include <vector>
 
 using namespace std::placeholders;
 
 int sum(int a, int b) {
     return a + b;
 }
+
+struct Counter {
+    int count = 0;
+    void operator()(int) {
+        count++;
+    }
+} ;
 
 int main(void) {
     auto plusFive = std::bind(sum, 5, _1);
@@ -19,5 +29,18 @@ int main(void) {
         std::bind(std::minus<int>(), _1, _2)
     );
 
-    std::cout << expr(5, 2);
+    std::cout << expr(5, 2) << std::endl;
+
+    std::vector<int> v{1, 2, 3, 4};
+    std::for_each(v.begin(), v.end(), [](int x){
+        std::cout << x << " ";
+    });
+    std::cout << std::endl;
+
+    Counter c = std::for_each(v.begin(), v.end(), Counter());
+    std::cout << c.count << std::endl;
+
+
+    int sum = std::accumulate(v.begin(), v.end(), 0);
+    std::cout << sum << std::endl;
 }
