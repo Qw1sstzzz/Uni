4899d1906b8bd5b13a6a253379b0d65665112268
Qwlsstzzz <clxwnhopelesss@yandex.ru>
2026-05-17 16:06:15 +0300

    Changed hash func to regular

--- Data_Structure/CourseWork/HashDictionary.cpp
+++ Data_Structure/CourseWork/HashDictionary.cpp
@@ -47,11 +47,7 @@ size_t HashDictionary::nextPrime(size_t n) {
 
 
 size_t HashDictionary::hash(const std::string& key) const {
-    size_t hashValue = 0;
-    for (char c : key) {
-        hashValue = hashValue * 31 + static_cast<size_t>(c);
-    }
-    return hashValue % size_;
+    return std::hash<std::string>{}(key) % size_;
 }
 
 
